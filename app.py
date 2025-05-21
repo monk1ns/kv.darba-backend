a771adf1b235aa81873c4985de332e2acc9633b8
monk1ns <d.slujev@gmial.com>
2025-05-21 14:50:51 +0300

    fix for deploy

--- app.py
+++ app.py
@@ -849,5 +849,8 @@ def export_pdf():
         return jsonify({"error": "PDF generation failed"}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
